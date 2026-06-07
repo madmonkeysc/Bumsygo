3a768050063b83c3822fd641495643e891180d20
Bumsy Go <bumsy@bumsygo.com>
2026-06-07 13:06:11 -0600

    style: Allow transparent header on /meet-and-play so hero image goes under menu bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const location = useLocation();
 
-  const isTransparentHeader = location.pathname === '/' || location.pathname === '/business' || location.pathname === '/watch' || location.pathname === '/shop' || location.pathname === '/pro';
+  const isTransparentHeader = location.pathname === '/' || location.pathname === '/business' || location.pathname === '/watch' || location.pathname === '/shop' || location.pathname === '/pro' || location.pathname === '/meet-and-play';
 
   useEffect(() => {
     const handleScroll = () => {
